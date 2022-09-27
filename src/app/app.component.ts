97a6642b3a857478e11ab7df56f375b6cbb6c48d
IcePyro <louis.haub@googlemail.com>
2022-09-27 15:04:47 +0200

    add test function to button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+function send_form(){
+  console.log("Sending Data");
+  Telegram.WebApp.sendData("Sent Data");
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +12,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'New Reminder';
+  send_form = send_form;
 }
